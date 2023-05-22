8a49d5c60676c64026d150d8ba7f25069a385033
Tobias Konzok <konzok.tobias@gmail.com>
2023-05-22 09:49:35 +0200

    Add gunicorn

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -11,7 +12,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['APP_SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
